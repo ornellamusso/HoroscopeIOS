609b0492c8f84d1ca62f52ad41c7030a26953c9f
ornella <ornellamussog@gmail.com>
2024-12-13 18:15:29 +0100

    Initial Commit

---
+++ HoroscopeIOSTests/HoroscopeIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoroscopeIOSTests.swift
+//  HoroscopeIOSTests
+//
+//  Created by Tardes on 13/12/24.
+//
+
+import Testing
+@testable import HoroscopeIOS
+
+struct HoroscopeIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
